812dd3ed6e1d9d8ba14f0cd2be495c9375b97360
konanki nithish kumar <nithishkumarkonanki@gmail.com>
2026-08-26 16:22:09 +0530

    changed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -45,12 +45,9 @@ userSchema.pre("save", async function () {
 
 userSchema.methods.comparePassword = async function (password) {
 
-    console.log(password, this.password)
-
     return await bcrypt.compare(password, this.password)
 
 }
-
 
 const userModel = mongoose.model("user", userSchema)
 
